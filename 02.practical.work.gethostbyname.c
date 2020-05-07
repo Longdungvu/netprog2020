d8c8bf7ff6269f08b958f7830005501ad48c1f3a
longdungvu <mltr85067@gmail.com>
2020-05-07 09:12:28 +0700

    fix typos and variables

--- 02.practical.work.gethostbyname.c
+++ 02.practical.work.gethostbyname.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
+ #include <netdb.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <arpa/inet.h>
+ #include <stdlib.h>
+ #include <string.h>
 
-int main() {
-    char x[999]
-	printf("hostname :")
-	scanf("%s", &x);
-    struct hostent *x = gethostbyname(hostname);
-    struct in_addr *in = (int*) domain->h_addr_list[0];
+ int main(int argc, char const *argv[]) {
+   char x[999];
+   if (argc == 1) {
+     printf("hostname :");
+     scanf("%s", x);
+   } else {
+     strcpy(x,argv[1]);
+   }
 
-    printf("ip address is %s\n",inet_ntoa(*in));
-}
+   struct hostent *domain = gethostbyname(x);
+   struct in_addr *in = (int*) domain->h_addr_list[0];
+   printf("ip address is %s\n",inet_ntoa(*in));
+   return 0;
+ }
