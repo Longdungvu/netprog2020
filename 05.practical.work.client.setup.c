12efea4570975701286986be6606f13b2e7e5976
vulongdung <mltr85067@gmail.com>
2020-05-11 12:10:08 +0700

    labwork 5 commit

---
+++ 05.practical.work.client.setup.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+
+int main() {
+  struct sockaddr_in saddr;
+  struct hostent *h;
+  int sockfd, receive;
+  unsigned short port = 2345;   //443 https default port
+  char buffer[1024] = {0};
+  if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("Error creting socket\n");
+    return -1;
+  }
+
+  if (( h = gethostbyname("127.0.0.1")) == NULL) {
+    printf("unknown host\n");
+    return -1;
+  }
+
+  memset(&saddr, 0 , sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+  saddr.sin_port = htons(port);
+  if (connect (sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+    printf("Cannot connect\n");
+    return -1;
+  } else {
+    printf("connection successful\n");
+    printf("hostname is %s\n", h->h_name);
+    struct in_addr *in = (int*) h->h_addr_list[0];
+    printf("ip address is %s\n",inet_ntoa(*in));
+
+    while (1) {
+      scanf("%s",buffer );
+      send(sockfd,buffer, 1024, 0);
+      receive = recv(sockfd, buffer, 1024, 0);
+      printf("output from server %s\n",buffer );
+    }
+  }
+}
---
+++ 05.practical.work.server.setup.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+int main() {
+  int sockfd, clen , clientfd, receive ;
+  char buffer[1024] = {0};
+  struct sockaddr_in saddr, caddr;
+  unsigned short port = 2345;
+  if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("error creating socket\n");
+  }
+
+
+  memset(&saddr, 0 , sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  saddr.sin_port = htons(port);
+
+
+  if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr))) < 0 ) {
+    printf("error listening\n");
+  }
+
+
+  if (listen(sockfd, 5) < 0) {
+    printf("error listening\n");
+  }
+
+  clen=sizeof(caddr);
+  if ((clientfd = accept(sockfd, (struct sockaddr*) &caddr, &clen)) < 0) {
+    printf("error accepting connection\n");
+  }
+  else{
+   printf("connection done\n");
+
+  char *ip = inet_ntoa(caddr.sin_addr);
+  printf("ip: %s\n", ip);
+
+  while (1) {
+    receive = recv(clientfd, buffer, 1024, 0);
+    printf("%s\n",buffer );
+    printf("say something: "); scanf("%s",buffer );
+    send(clientfd,buffer, 1024, 0);
+    }
+  }
+}
