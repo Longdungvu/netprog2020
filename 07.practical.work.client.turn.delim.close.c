18a205122cfcdaf7935dae2b3d6fb7ad37fd408a
slowpoke <57148919+Longdungvu@users.noreply.github.com>
2020-05-21 10:44:12 +0700

    commit labwork 7

---
+++ 07.practical.work.client.turn.delim.close.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int isDelimiter(char* x) {
+  for (int i = 0; i < strlen(x); i++) {
+    if (x[i] == ' ') {
+      return 1;
+    }
+  }
+  return 0;
+ }
+
+int main() {
+  struct sockaddr_in saddr;
+  struct hostent *h;
+  int sockfd, receive;
+  unsigned short port = 123;   //443 https default port
+  char buffer[1024] = {0};
+  char data[2024] = {0};
+  char apd = ' ';
+  if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("Error creting socket\n");
+    exit(0);
+  }
+
+  if (( h = gethostbyname("127.0.0.1")) == NULL) {
+    printf("unknown host\n");
+    return -1;
+  }
+
+  memset(&saddr, 0 , sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+  saddr.sin_port = htons(port);
+
+  if (connect (sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+    printf("Cannot connect\n");
+    return -1;
+  } else {
+    printf("connection successful\n");
+    printf("hostname is %s\n", h->h_name);
+    struct in_addr *in = (struct in_addr*) h->h_addr_list[0];
+    printf("ip address is %s\n",inet_ntoa(*in));
+
+    while (1) {
+      //send messages to server
+      memset(&buffer, 0 , sizeof(buffer));
+      memset(&data, 0 , sizeof(data));
+      printf("[Client]: ");
+      scanf("%s",buffer);
+      //disconecting out of Server
+      if (strcmp(buffer,"/quit") == 0) {
+        printf("disconnecting\n");
+        close(sockfd);
+        exit(0);
+      }
+      strncat(buffer,&apd,1); //add Delimiter to messages
+      send(sockfd,buffer, sizeof(buffer), 0);
+      //receive server messages
+      memset(&buffer, 0 , 1024);
+      memset(&data, 0 , 2024);
+      while(isDelimiter(buffer) == 0) {
+      receive = recv(sockfd, buffer, sizeof(buffer), 0);
+      //ACK from Server
+      if (receive <= 0) {
+        printf("server disconecting you\n");
+        close(sockfd);
+        exit(0);
+      }
+      strncat(data,buffer,strlen(buffer));
+      }
+      printf("[Server]: %s",data );
+
+
+    }
+  }
+}
---
+++ 07.practical.work.server.turn.delim.close.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
+
+int isDelimiter(char* x) {
+  for (int i = 0; i < strlen(x); i++) {
+    if (x[i] == ' ') {
+      return 1;
+    }
+  }
+  return 0;
+ }
+
+int main() {
+  int sockfd, clen , clientfd, receive ;
+  char buffer[1024] = {0};
+  char data[2024] = {0};
+  char apd = ' ';
+  struct sockaddr_in saddr, caddr;
+  unsigned short port = 123;
+  if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("error creating socket\n");
+    exit(0);
+  }
+
+
+  memset(&saddr, 0 , sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  saddr.sin_port = htons(port);
+
+
+  if ((bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr))) < 0 ) {
+    printf("error listening\n");
+    exit(0);
+  }
+
+
+  if (listen(sockfd, 5) < 0) {
+    printf("error listening\n");
+    exit(0);
+  }
+  accept:
+  clen=sizeof(caddr);
+  if ((clientfd = accept(sockfd, (struct sockaddr*) &caddr,(socklen_t*) &clen)) < 0) {
+    printf("error accepting connection\n");
+    exit(0);
+  }
+  else{
+   printf("connection done\n");
+
+  char *ip = inet_ntoa(caddr.sin_addr);
+  printf("ip: %s\n", ip);
+
+
+
+  while (1) {
+    //receive client messages
+    memset(&buffer, 0 , sizeof(buffer));
+    memset(&data, 0 , sizeof(data));
+
+    while (isDelimiter(buffer) == 0) {
+    receive = recv(clientfd, buffer, sizeof(buffer), 0);
+    //ACK client disconecting
+    if (receive <= 0) {
+      printf("client disconnected\n");
+      close(clientfd);
+      goto accept;
+    }
+
+    strncat(data,buffer,strlen(buffer)); //append to buffer
+    }
+
+    printf("[Client]: %s",data );
+
+    //send messages to client
+    memset(buffer, 0 , 1024);
+    memset(data, 0 , 2024);
+
+    printf("[Server]: ");
+    scanf("%s",buffer);
+
+    //disconecting client
+    if (strcmp(buffer,"/cd") == 0) {
+      printf("disconecting client\n");
+      shutdown(clientfd,SHUT_RDWR);
+      int count;
+      char c;
+      while ((count = read(clientfd, &c, sizeof(c))) > 0);
+      close(clientfd);
+      goto accept;
+    }
+
+    strncat(buffer,&apd,1);
+    send(clientfd,buffer, sizeof(buffer), 0);
+
+    }
+  }
+
+
+}
